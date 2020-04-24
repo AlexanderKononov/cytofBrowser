4eb590a60ee4a589b5ad0cf0af1f9997b2ca044f
AlexanderKononov <Kononov.nov@mail.ru>
2020-04-24 15:33:08 +0100

    add tests to data preparation

--- tests/testthat/test_main_pipeline.R
+++ tests/testthat/test_main_pipeline.R
@@ -6,6 +6,28 @@ test_that(desc = "Check the main pipeline from upload data to clustering", code 
                   "KPC6_DNA_viSNE_CD45-_stroma.fcs", "KPC7_DNA_viSNE_CD45-_stroma.fcs",
                   "KPC8_DNA_viSNE_CD45-_stroma.fcs")
   test_files <- system.file("extdata",test_files,package = "cytofBrowser")
-  #cytofBrowser::get_fcs_metadata(test_files)
+
+  md <- cytofBrowser::get_fcs_metadata(test_files)
+  expect_equal(dim(md), c(7,3), info = "Error in function: get_fcs_metadata")
+
+  fcs_raw <- cytofBrowser::get_fcs_raw(md)
+  expect_equal(length(fcs_raw), 7, info = "Error in function: get_fcs_raw")
+
+  panel <- cytofBrowser::get_fcs_panel(fcs_raw)
+  expect_equal(dim(panel), c(54,4), info = "Error in function: get_fcs_panel")
+  expect_equal(all(panel$marker_class %in% c("type", "state")), TRUE,
+               info = "Error in function: get_fcs_panel. problem with marker_class")
+
+  use_markers <- cytofBrowser::get_use_marker(panel)
+  expect_equal(all(c(use_markers %in% panel$name, names(use_markers) %in% panel$antigen)), TRUE,
+               info = "Error in function: get_use_marker")
+
+  fcs_asinh <- cytofBrowser::asinh_transformation(fcs_raw)
+  expect_equal(length(fcs_asinh) == length(fcs_raw),TRUE, info = "Error in function: asinh_transformation")
+  expect_equal(dim(fcs_asinh[[1]]) == dim(fcs_raw[[1]]),TRUE, info = "Error in function: asinh_transformation")
+  fcs_raw <- fcs_asinh
+  rm(fcs_asinh)
+
+
 })
 
